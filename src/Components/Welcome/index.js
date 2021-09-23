b83c8c8dc2a18d8b902e1198861bc331a3b8feb1
AnthonySzymczak <doyalajs1@live.com>
2021-09-23 19:29:54 -0400

    for images

--- src/Components/Welcome/index.js
+++ src/Components/Welcome/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import Login from "../Login/Login";
 import Signup from "../Login/Signup";
 import "./Welcome.css";
-
+import coverImage from "../../images/Bar.jpg"
 function Welcome() {
   return (
-    <div class="Welcome1">
+    <div class="Welcome1"  >
       <h1>Cocktail Dreamer</h1>
       <div class="Welcome2">
         <h3>Welcome to Cocktail Dreamer, Login to get started.</h3>
         <Login />
         <Signup />
       </div>
+      <img src={coverImage} className="my-2" style={{ width: "60%" }} alt="cover" />
     </div>
   );
 }
