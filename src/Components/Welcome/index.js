73e42fbba4297803bc9462690bfc7b38938807dd
AnthonySzymczak <doyalajs1@live.com>
2021-09-23 13:40:11 -0400

    Going crazy here

--- src/Components/Welcome/index.js
+++ src/Components/Welcome/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import Login from "../Login/Login";
+import Signup from "../Login/Signup";
 
-function Welcome(){
-    return(
-        <div>
-            <h1>Cocktail Dreamer</h1>
-            <div>
+function Welcome() {
+  return (
+    <div class ="">
+      <h1>Cocktail Dreamer</h1>
+      <div>
                 <h3>Welcome to Cocktail Dreamer, Login to get started.</h3>
+                <Login />
+                <Signup />
+              </div>
             </div>
-        </div>
-    )
+       
+  );
 }
 export default Welcome;
