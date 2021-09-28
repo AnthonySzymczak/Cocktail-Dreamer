ba2d8ee189dc01946ac3cc9fad6dd1ac3527c13a
Anthony Szymczak <doyalajs1@live.com>
2021-09-28 16:55:23 -0400

    links to pages?

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Cocktails from './Components/Cocktails/Cocktails';
 import ContactForm from './Components/Contact/index';
 import Create from './Components/Create';
-import Knowledge from './Components/Booze/Knowledge';
+import Knowledge from './Components/Booze/Knowledge/index';
 import Suggestions from './Components/Suggestions/Suggestions';
 import AboutUs from './Components/AboutUs';
 import Welcome from './Components/Welcome';
--- src/Components/Booze/Knowledge/index.js
+++ src/Components/Booze/Knowledge/index.js
@@ -5,14 +5,6 @@ import ImageV from '../../../images/Vodka.jpg'
 import ImageW from '../../../images/Whiskey.jpg'
 import ImageR from '../../../images/Rum.jpg'
 import ImageT from '../../../images/Tequila.jpg'
-
-<BrandyCard
-imgAlt="Bottles of Brandy"
-eyebrow="Brandy"
-title="Ingredients, Types, and History"
-url="../Brandy"
-/>
-
 /*function Template({ children, ...attrs }) {
     return (
       <template
@@ -21,40 +13,91 @@ url="../Brandy"
       />
     );
   } */
-
 function Knowledge() {
     return (
-      
+      <>
         <div><h3>
-                This page is where we incorporate the seven kowledge-based pages into cards 
-                to be clicked on and then bring you to the corresponding pages. 
+                Scroll down and read up on some of the most popular drinks around the world.
         </h3></div>
-     /*   <div class="flex items-center justify-between w-full">
-        <div class="flex flex-col lg:flex-row w-full items-start lg:items-center rounded bg-white shadow">
-            <div class="w-full lg:w-2/3 h-64 dark:bg-gray-800">Let me Test</div>
-            <div class="w-full lg:w-1/3 h-24 dark:border-gray-700 lg:h-64 border-t lg:border-t-0 lg:border-r lg:border-l lg:rounded-r dark:bg-gray-700 bg-gray-100">The text here</div>
-        </div> */
         
   <div>
-    <img class="rounded" src={ImageB} alt="imgAlt"/>
-    <div className="mt-2">
+    <img className="photo" src={ImageB} alt="Brandy bottle"/>
+    <div className='mt-2'>
       <div>
-        <div className="text-xs text-blue-600 uppercase font-bold">{ eyebrow }</div>
-        <div className="font-bold text-blue-700 leading-snug">
-          <a href="url" class="hover:underline">{ title }</a>
-        </div>
+        <h2 className="justify-center text-xs text-blue-600 uppercase font-bold">Brandy
+        </h2>
+        <h1 className="font-bold text-blue-700 leading-snug">
+          <a href="url" className="hover:underline">Ingredients, Types, and History</a>
+        </h1>
       </div>
     </div>
   </div>
-)
-}
-
-export default {
-    props: ['img', 'imgAlt', 'eyebrow', 'title', 'pricing', 'url']
-  }
+  <div>
+    <img className="photo" src={ImageG} alt="Gin bottle"/>
+    <div className='mt-2'>
+      <div>
+        <h2 className="justify-center text-xs text-blue-600 uppercase font-bold">Gin
+        </h2>
+        <h1 className="font-bold text-blue-700 leading-snug">
+          <a href="url" className="hover:underline">Ingredients, Types, and History</a>
+        </h1>
+      </div>
+    </div>
+  </div>
+  <div>
+    <img className="photo" src={ImageV} alt="Vodka bottle"/>
+    <div className='mt-2'>
+      <div>
+        <h2 className="justify-center text-xs text-blue-600 uppercase font-bold">Vodka
+        </h2>
+        <h1 className="font-bold text-blue-700 leading-snug">
+          <a href="url" className="hover:underline">Ingredients, Types, and History</a>
+        </h1>
+      </div>
+    </div>
+  </div>
+  <div>
+    <img className="photo" src={ImageT} alt="Tequila bottle"/>
+    <div className='mt-2'>
+      <div>
+        <h2 className="justify-center text-xs text-blue-600 uppercase font-bold">Tequila
+        </h2>
+        <h1 className="font-bold text-blue-700 leading-snug">
+          <a href="url" className="hover:underline">Ingredients, Types, and History</a>
+        </h1>
+      </div>
+    </div>
+  </div>
+  <div>
+    <img className="photo" src={ImageR} alt="Rum bottle"/>
+    <div className='mt-2'>
+      <div>
+        <h2 className="justify-center text-xs text-blue-600 uppercase font-bold">Rum
+        </h2>
+        <h1 className="font-bold text-blue-700 leading-snug">
+          <a href="url" className="hover:underline">Ingredients, Types, and History</a>
+        </h1>
+      </div>
+    </div>
+  </div>
+  <div>
+    <img className="photo" src={ImageW} alt="Whiskey bottle"/>
+    <div className='mt-2'>
+      <div>
+        <h2 className="justify-center text-xs text-blue-600 uppercase font-bold">Whiskey
+        </h2>
+        <h1 className="font-bold text-blue-700 leading-snug">
+          <a href="url" className="hover:underline">Ingredients, Types, and History</a>
+        </h1>
+      </div>
+    </div>
+  </div>
+  </>
   );
 };
-
-
-
+     /*   <div className="flex items-center justify-between w-full">
+        <div className="flex flex-col lg:flex-row w-full items-start lg:items-center rounded bg-white shadow">
+            <div className="w-full lg:w-2/3 h-64 dark:bg-gray-800">Let me Test</div>
+            <div className="w-full lg:w-1/3 h-24 dark:border-gray-700 lg:h-64 border-t lg:border-t-0 lg:border-r lg:border-l lg:rounded-r dark:bg-gray-700 bg-gray-100">The text here</div>
+        </div> */
 export default Knowledge;
